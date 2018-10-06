0c5b1afc030ee58d422ceb2ff84b62be5f828bea
JuhaLuukkonen <32368550+JuhaLuukkonen@users.noreply.github.com>
2018-10-06 11:43:33 +0300

    Add files via upload

---
+++ NumeronArvaajaPython.py
@@ -0,0 +1,34 @@
+# Numeronarvauspeli, koodi muokaten verkosta
+import random
+
+guessesTaken = 0
+
+print('Hei! Mikä on nimesi?')
+myName = input()
+
+number = random.randint(1, 20)
+
+print('Pelataan!, ' + myName + 'Arvaa luku 1-20 väliltä?')
+
+while guessesTaken < 6:
+    print('Arvaa?')
+    guess = input()
+    guess = int(guess)
+
+    guessesTaken = guessesTaken + 1
+    if guess < number:
+        print('Arvauksesi on liian pieni')
+
+    if guess > number:
+        print('Arvauksesi on liian suuri')
+
+    if guess == number:
+        break
+    
+if guess == number:
+    guessesTaken = str(guessesTaken)
+    print('Hyvä!!! ' + myName + '! Arvasit ajatukseni ' + guessesTaken + ' Pelottavaa!')
+
+if guess != number:
+    number = str(number)
+    print('Ei, ikävä kyllä väärä numero' + number)
